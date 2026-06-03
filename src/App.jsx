460c7c76dd403cd9c6d02125a730af17d7a71da6
Jhosep2408 <trabajosgithub@gmail.com>
2026-06-02 22:30:53 -0500

    Actualización forzada

--- src/App.jsx
+++ src/App.jsx
@@ -570,7 +570,7 @@ export default function App() {
                 Gracias por compartir tantos momentos conmigo ❤️
               </h1>
               <p className="final-subtext">
-                He creado este pequeño universo para celebrar tu día. Las fotos que orbitan Saturno representan los hermosos momentos que hemos vivido juntos.
+                He creado este pequeño universo para celebrar tu día. Las fotos que orbitan Saturno representan los hermosos momentos que hemos vivido juntos. Gracias
               </p>
               <button 
                 className="btn-premium" 
